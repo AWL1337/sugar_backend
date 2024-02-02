5efb52b7502edcf443a471ef4958a684b02a085a
lipipidron <lociX55lim@gmail.com>
2024-02-02 17:50:49 +0300

    feat: user repo func

--- Sugar_backend.Infrastructure.DataAccess/Repositories/UserRepository.cs
+++ Sugar_backend.Infrastructure.DataAccess/Repositories/UserRepository.cs
@@ -1,3 +1,6 @@
+using Itmo.Dev.Platform.Postgres.Connection;
+using Itmo.Dev.Platform.Postgres.Extensions;
+using Npgsql;
 using Sugar_backend.Application.Abstractions.Repositories;
 using Sugar_backend.Application.Models.Users;
 
@@ -5,6 +8,13 @@ namespace Sugar_backend.Infrastructure.DataAccess.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly IPostgresConnectionProvider _connectionProvider;
+
+    public UserRepository(IPostgresConnectionProvider connectionProvider)
+    {
+        _connectionProvider = connectionProvider;
+    }
+
     public User? AddUser(string login, string password, string name, DateTime birthday, Gender gender, int weight,
         int carbohydrateRatio, int breadUnit)
     {
@@ -16,14 +26,49 @@ public class UserRepository : IUserRepository
         throw new NotImplementedException();
     }
 
-    public User FindUserByLogin(string login)
+    public User? FindUserByLogin(string login)
     {
-        throw new NotImplementedException();
+        const string sql = """
+                           select *
+                           from users
+                           where login = :login;
+                           """;
+        var connection = _connectionProvider
+            .GetConnectionAsync(default)
+            .GetAwaiter()
+            .GetResult();
+
+        using var command = new NpgsqlCommand(sql, connection)
+            .AddParameter("login", login);
+
+        using var reader = command.ExecuteReader();
+
+        if (reader.Read() is false)
+            return null;
+
+        return new User(
+            reader.GetInt64(0),
+            reader.GetString(1),
+            reader.GetFieldValue<UserInfo>(2));
     }
 
     public bool ChangeName(string login, string newName)
     {
-        throw new NotImplementedException();
+        var userId = FindUserByLogin(login)?.Id;
+        const string sql = "UPDATE user_info SET name = :newName where user_id = :userId";
+
+        var connection = _connectionProvider
+            .GetConnectionAsync(default)
+            .GetAwaiter()
+            .GetResult();
+
+        using var command = new NpgsqlCommand(sql, connection)
+            .AddParameter("newName", newName)
+            .AddParameter("userId", userId);
+
+        command.ExecuteNonQueryAsync();
+
+        return true;
     }
 
     public bool ChangeBirthday(string login, DateTime newBirthday)
@@ -38,16 +83,58 @@ public class UserRepository : IUserRepository
 
     public bool ChangeWeight(string login, int newWeight)
     {
-        throw new NotImplementedException();
+        var userId = FindUserByLogin(login)?.Id;
+        const string sql = "UPDATE user_info SET weight = :newWeight where user_id = :userId";
+
+        var connection = _connectionProvider
+            .GetConnectionAsync(default)
+            .GetAwaiter()
+            .GetResult();
+
+        using var command = new NpgsqlCommand(sql, connection)
+            .AddParameter("newWeight", newWeight)
+            .AddParameter("userId", userId);
+
+        command.ExecuteNonQueryAsync();
+
+        return true;
     }
 
     public bool ChangeCarbohydrateRatio(string login, int newCarbohydrateRatio)
     {
-        throw new NotImplementedException();
+        var userId = FindUserByLogin(login)?.Id;
+        const string sql = "UPDATE user_info SET carbohydrate_ratio = :newCarbohydrateRatio where user_id = :userId";
+
+        var connection = _connectionProvider
+            .GetConnectionAsync(default)
+            .GetAwaiter()
+            .GetResult();
+
+        using var command = new NpgsqlCommand(sql, connection)
+            .AddParameter("newCarbohydrateRatio", newCarbohydrateRatio)
+            .AddParameter("userId", userId);
+
+        command.ExecuteNonQueryAsync();
+
+        return true;
     }
 
     public bool ChangeBreadUnit(string login, int newBreadUnit)
     {
-        throw new NotImplementedException();
+        var userId = FindUserByLogin(login)?.Id;
+        const string sql = "UPDATE user_info SET bread_unit = :newBreadUnit where user_id = :userId";
+
+        var connection = _connectionProvider
+            .GetConnectionAsync(default)
+            .GetAwaiter()
+            .GetResult();
+
+        using var command = new NpgsqlCommand(sql, connection)
+            .AddParameter("newBreadUnit", newBreadUnit)
+            .AddParameter("userId", userId);
+
+        command.ExecuteNonQueryAsync();
+
+        return true;
     }
 }
