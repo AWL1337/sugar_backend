95fc59d7d5d3aab1eeefb762afa43601b472d527
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-02-03 21:29:28 +0300

    feat: added Extensions and Plugins

---
+++ Sugar_backend.Infrastructure.DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+using Itmo.Dev.Platform.Postgres.Extensions;
+using Itmo.Dev.Platform.Postgres.Models;
+using Itmo.Dev.Platform.Postgres.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+using Sugar_backend.Application.Abstraction.Repositories;
+using Sugar_backend.Application.Abstractions.Repositories;
+using Sugar_backend.Infrastructure.DataAccess.Repositories;
+
+namespace Sugar_backend.Infrastructure.DataAccess.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureDataAccess(
+        this IServiceCollection collection,
+        Action<PostgresConnectionConfiguration> configuration)
+    {
+        collection.AddPlatformPostgres(builder => builder.Configure(configuration));
+        collection.AddPlatformMigrations(typeof(ServiceCollectionExtensions).Assembly);
+
+        collection.AddSingleton<IDataSourcePlugin, MappingPlugin>();
+
+        collection.AddScoped<IUserRepository, UserRepository>();
+        collection.AddScoped<INoteRepository, NoteRepository>();
+        collection.AddScoped<IProductRepository, ProductRepository>();
+
+        return collection;
+    }
+}
---
+++ Sugar_backend.Infrastructure.DataAccess/Plugings/MappingPlugin.cs
@@ -0,0 +1,15 @@
+using Itmo.Dev.Platform.Postgres.Plugins;
+using Npgsql;
+using Sugar_backend.Application.Models.Notes;
+using Sugar_backend.Application.Models.Users;
+
+namespace Sugar_backend.Infrastructure.DataAccess.Extensions;
+
+public class MappingPlugin : IDataSourcePlugin
+{
+    public void Configure(NpgsqlDataSourceBuilder builder)
+    {
+        builder.MapEnum<NoteType>();
+        builder.MapEnum<Gender>();
+    }
+}
